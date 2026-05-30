1750f76cbae0ed851b24881d4dddf48b866945b8
luciensouthard-LSDS <lucien.southard@gmail.com>
2026-05-29 23:00:08 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -6810,7 +6810,7 @@ export default async function handler(req, res) {
       parts: [{ text: m.content }],
     }));
 
-    const geminiUrl = 'https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=' + geminiKey;
+    const geminiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=' + geminiKey;
 
     const response = await fetch(geminiUrl, {
       method: 'POST',
